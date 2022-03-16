52181945508e73450ad0dd631474363edfea461e
Arnau Sanz Froiz <arnau.sf@students.salle.url.edu>
2022-03-16 01:01:20 +0100

    Delivery 5 finished. Bonus: Add Task button is on Toolbar :D

--- src/main/java/edu/url/salle/arnau/sf/todolist/MainActivity.java
+++ src/main/java/edu/url/salle/arnau/sf/todolist/MainActivity.java
@@ -1,18 +1,24 @@
 package edu.url.salle.arnau.sf.todolist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,14 +37,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        //toolbar.inflateMenu(R.menu.action_bar_main); tried to -not- need onCreateOptionsMenu() but failed miserably. Leaving it as hope (or warning) for the brave Arnau of the future.
+        setSupportActionBar(toolbar);
+
         mTaskRecyclerView = findViewById(R.id.list_recycler_view);
         mTaskRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_addTask) {
+                startActivityForResult(new Intent(MainActivity.this, TaskCreationActivity.class), REQUEST_TASK);
+                return true;
+            }
+            return super.onMenuItemSelected(item.getItemId(), item); //honestly the first parameter is probably wrong but this method is the closest I found to super.onOptionsItemSelected() from Android docs tutorial on actionBar actions.
+        });
+
+        //adding initial tasks to avoid ugly empty list
         rTaskList.add(new Task("Call mum"));
         rTaskList.add(new Task("Deliver Android AC"));
 
         updateUI();
-        startActivityForResult(new Intent(MainActivity.this, TaskCreationActivity.class), REQUEST_TASK);
+        //startActivityForResult(new Intent(MainActivity.this, TaskCreationActivity.class), REQUEST_TASK);
     }
 
     private void updateUI() {
@@ -46,8 +65,38 @@ public class MainActivity extends AppCompatActivity {
         mTaskRecyclerView.setAdapter(mTaskAdapter);
     }
 
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode  != Activity.RESULT_OK) return;
+        if (requestCode == REQUEST_TASK) {
+            if (intent == null) return;
+            rTaskList.add(TaskCreationActivity.getTaskFromIntent(intent));
+            updateUI();
+            Toast.makeText(MainActivity.this, R.string.toast_task_created, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_main, menu);
+        return true;
+    }
+/*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_addTask:
+                Toast.makeText(this, "new Task!", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+*/
     private class TaskAdapter extends RecyclerView.Adapter<TaskHolder> {
-        private List<Task> rTasks;
+        private final List<Task> rTasks;
 
         public TaskAdapter (List<Task> tasks) {
             rTasks = tasks;
@@ -70,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private static class TaskHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private Task mTask;
 
         private TextView txtviewTitle;
@@ -79,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
 
         public TaskHolder (LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.list_item_task, parent, false));
-            itemView.setOnClickListener(this);
+            //itemView.setOnClickListener(this);
 
             txtviewTitle = itemView.findViewById(R.id.task_title);
             txtviewDate = itemView.findViewById(R.id.task_dueDate);
@@ -94,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
             mTask = task;
             txtviewTitle.setText(mTask.getTitle());
             txtviewDate.setText(mTask.getDate().toString());
+            checkboxStatus.setChecked(mTask.isCompleted());
         }
 
         @Override
--- src/main/java/edu/url/salle/arnau/sf/todolist/TaskCreationActivity.java
+++ src/main/java/edu/url/salle/arnau/sf/todolist/TaskCreationActivity.java
@@ -1,14 +1,62 @@
 package edu.url.salle.arnau.sf.todolist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class TaskCreationActivity extends AppCompatActivity {
+
+    private static final String EXTRA_TASK_NAME = "TASK_NAME";
+    private static final String TAG = "WHAT";
+    private EditText edittxtTaskName;
+    private Button bttnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_creation);
+
+        Toolbar toolbar = findViewById(R.id.task_toolbar);
+        setSupportActionBar(toolbar);
+
+        edittxtTaskName = findViewById(R.id.edittxt_input_task_name);
+        bttnAddTask = findViewById(R.id.button_addTask);
+
+        bttnAddTask.setOnClickListener(v -> addTask());
+
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.action_addTask) {
+                //startActivityForResult(new Intent(MainActivity.this, TaskCreationActivity.class), REQUEST_TASK);
+                addTask();
+                return true;
+            }
+            return super.onMenuItemSelected(item.getItemId(), item); //honestly the first parameter is probably wrong but this method is the closest I found to super.onOptionsItemSelected() from Android docs tutorial on actionBar actions.
+        });
+    }
+
+    private void addTask() {
+        setResult(RESULT_OK, new Intent().putExtra(EXTRA_TASK_NAME, edittxtTaskName.getText().toString()));
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_main, menu);
+        return true;
+    }
+
+    public static Task getTaskFromIntent(@NonNull Intent intent) {
+        return new Task(intent.getStringExtra(EXTRA_TASK_NAME));
     }
 }
